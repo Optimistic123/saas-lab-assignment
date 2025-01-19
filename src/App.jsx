4c1146542be1a4682efc3539130e572ef2d5bd20
Optimistic123 <manishkj441@gmail.com>
2025-01-19 23:57:27 +0530

    laoding state added

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,19 @@ const API_URL =
   "https://raw.githubusercontent.com/saaslabsco/frontend-assignment/refs/heads/master/frontend-assignment.json";
 
 function App() {
+  const [loading, setLoading] = useState(false);
   const [projects, setProjects] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
   const recordsPerPage = 5;
 
   useEffect(() => {
+    setLoading(true);
     fetch(API_URL)
       .then((response) => response.json())
-      .then((data) => setProjects(data))
+      .then((data) => {
+        setProjects(data);
+        setLoading(false);
+      })
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
@@ -29,13 +34,14 @@ function App() {
     <div className="App">
       <div className="content-container">
         <h1>Kickstarter Projects</h1>
+        {loading ? <div>Loading .....</div>:
         <CustomTable
           currentRecords={currentRecords}
           firstIndex={firstIndex}
           totalPages={totalPages}
           handlePageChange={handlePageChange}
           currentPage={currentPage}
-        />
+        />}
       </div>
     </div>
   );
